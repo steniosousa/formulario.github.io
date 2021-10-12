6cd6940010258273cdfaecf2fb9b24730f9b53f6
stenio sousa <81321894+steniosousa@users.noreply.github.com>
2021-10-12 13:42:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const enviar = document.querySelector("#enviar");
-enviar.addEventListener("click", function(stop) {
-    stop.preventDefault();
+enviar.addEventListener("click", function(e) {
+    e.preventDefault();
     const name = document.querySelector("#name");
     const value = name.value;
     const concatenacao =  "Seja bem vindo, "+ value + "!";
